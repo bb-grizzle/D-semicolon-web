0aedbf2db8467477bbdf7bb889f41c0be7f0dead
grizzle <firbigi1993@gmail.com>
2020-07-18 19:18:38 +0900

    add : footer

--- src/com/section/Footer.jsx
+++ src/com/section/Footer.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import logo from "image/logo/D_logo_w.svg";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <footer className="Footer">
-      <div className="con">footer</div>
-    </footer>
-  );
+	return (
+		<footer className="Footer">
+			<div className="con">
+				<div className="col">
+					<div className="logo">
+						<img src={logo} alt="logo" />
+					</div>
+					<nav>
+						<ul>
+							<li>
+								<NavLink to={"/"}>about</NavLink>
+							</li>
+							<li>
+								<NavLink to={"/study"}>study</NavLink>
+							</li>
+							<li>
+								<NavLink to={"/member"}>member</NavLink>
+							</li>
+							<li>
+								<NavLink to={"/contact"}>contact</NavLink>
+							</li>
+						</ul>
+					</nav>
+					<p className="copyright">Copyright © 2019 Kunkok University _ D-semicolon</p>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/com/section/HomeOverview.jsx
+++ src/com/section/HomeOverview.jsx
@@ -3,23 +3,22 @@ import React from "react";
 import { SectionTitle } from "com/component";
 
 const HomeOverview = () => {
-  return (
-    <div className="HomeOverview">
-      <div className="con">
-        <SectionTitle type={"text"} title="We Are - " link={"/study"} />
-        <div className="wrap-text">
-          <p className="text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-          </p>
-        </div>
-        <div className="wrap-img"></div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="HomeOverview">
+			<div className="con">
+				<SectionTitle type={"text"} title="We Are - " link={"/study"} />
+				<div className="wrap-text">
+					<p className="text">
+						It is hard to access UI/UX design field think without programming technology. In addition, it is difficult to realization our own design. However, the output that designers can make by
+						stepping into programming are incredible. Create your own design works, from simple webpage deployments to interactive graphics. <br />
+						<br />
+						Join us!
+					</p>
+				</div>
+				<div className="wrap-img"></div>
+			</div>
+		</div>
+	);
 };
 
 export default HomeOverview;
